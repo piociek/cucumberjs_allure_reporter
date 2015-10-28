8c227bcf10dcdee5dc4bc295596ac1b72ebcd3bf
Piotr Michalski <pio.michalski@gmail.com>
2015-10-28 10:37:48 +0100

    * fixed missing import

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ var Reporter = require('./src/CucumberJSAllureReporter');
 var Runtime = require('allure-js-commons/runtime');
 
 global.allure = new Runtime(Reporter.allureReporter);
+module.exports = Reporter;
